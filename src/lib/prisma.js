3c281518475d3bcea9b543fdb5dd52b081e543a8
VINEETHAVIJAYAN <vinusugathen@gmail.com>
2025-08-14 19:48:16 +0530

    prisma erroro corrected

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,13 +1,3 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis
-
-// Prevent multiple instances of Prisma Client in development
-export const prisma = globalForPrisma.prisma || 
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  })
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+export const prisma = new PrismaClient()
